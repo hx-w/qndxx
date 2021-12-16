2bd6fefde943bf7d54057a76e8f2c8e33ff3509c
hx-w <herixth@163.com>
2021-12-16 10:43:31 +0800

    +static image

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="image-div" :style="{backgroundImage: 'url(' + dxx + ')'}">
   </div>
 </template>
 
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      dxx: "https://h5.cyol.com/special/daxuexi/bsz1fk1u3w/images/end.jpg",
+    };
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.image-div {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: 100% 100%;
 }
 </style>
