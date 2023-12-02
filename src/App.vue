e9abc4a9f76ffc0a1dc91ae1bb82debf61e53368
carol <herixth@163.com>
2023-12-02 13:46:02 +0800

    [add] 添加输入框

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@
     id="app"
     class="image-div"
     :style="{ backgroundImage: 'url(' + result + ')' }"
-  ></div>
+  >
+    <div v-if="show">
+      <p>请输入当前期号，形如"2023年第21期"</p>
+      <input v-model="user_input">
+      <button v-on:click="submit">确认</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,8 +20,9 @@ export default {
     const url = window.location.href
     return {
       api_endpoint: url.split('?')[0] + '/.netlify/functions/qndxx',
-      title: '请使用[?id=20xx年第xx期]的形式访问',
-      result: 'https://www.example.com'
+      title: '请在弹窗中输入标题',
+      result: 'https://www.example.com',
+      show: true
     };
   },
   mounted() {
@@ -24,6 +31,7 @@ export default {
       .then((resp) => {
         if (this.$route.query.id) {
           this.title = '“青年大学习”' + this.$route.query.id
+          this.show = false
         }
         this.result = resp.data.result
       })
@@ -40,6 +48,12 @@ export default {
       },
     };
   },
+  methods: {
+    submit: function () {
+      this.title = '"青年大学习"' + this.user_input
+      this.show = false
+    }
+  }
 };
 </script>
 
